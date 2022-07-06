4af96fdd88e90bcdad309e93da14a907bbe93840
netoabel <abel.neto@gmail.com>
2022-07-06 17:01:01 -0300

    Merges linux and macos scripts into a single file

--- camera-bind/camera-bind.sh
+++ camera-bind/camera-bind.sh
@@ -1,11 +1,50 @@
 #!/bin/bash
+currentPath=$( cd "$(dirname "${BASH_SOURCE[0]}")" || exit; pwd -P )
+cd "$currentPath" || exit
+
+currentStatus="Off"
+
 OS="`uname`"
 case $OS in
   'Linux')
-    /bin/bash ./camera-bind-linux.sh
+    OS='linux'
     ;;
   'Darwin') 
-    /bin/bash ./camera-bind-macos.sh
+    OS='macos'
     ;;
-  *) ;;
+  *) 
+    echo "Unsupported Operating System."
+    exit;
+  ;;
 esac
+
+isCameraOnMac(){
+    cameraActivityLog=$(log show --predicate 'subsystem contains "com.apple.UVCExtension" and composedMessage contains "Post PowerLog"' --last 3m)
+    lastSixChars=${cameraActivityLog: -6}
+    cameraStatus=$(echo $lastSixChars | tr -cd '[:alnum:]._-')
+    [ $cameraStatus = "Off" ]; isCameraOn=$?
+    echo "$isCameraOn"
+}
+
+isCameraOnLinux(){
+    cameraActivityLog=$(lsof /dev/video0)
+    [ -z "$cameraActivityLog" ]; isCameraOn=$?
+    echo "$isCameraOn"
+}
+
+while true
+do
+    isCameraOn=$([ "$OS" = "macos" ] && echo "$(isCameraOnMac)" || echo "$(isCameraOnLinux)" )
+
+    if [[ "$isCameraOn" = 1 ]] && [[ $currentStatus = "Off" ]];
+    then
+        /bin/bash ../switch/on.sh
+        currentStatus="On"
+    elif [[ "$isCameraOn" = 0 ]] && [[ $currentStatus = "On" ]];
+    then
+        /bin/bash ../switch/off.sh
+        currentStatus="Off"
+    fi
+
+    sleep 1
+done
