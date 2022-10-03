931b60a51403c42ff189b20b82fcdbe4575458bf
abel <abel@abels-MacBook-Pro.local>
2022-10-03 17:57:36 -0300

    Fixes a bug that was causing the keylight to turn on automatically when there weren't any changes in camera status for more than 3 minutes

--- camera-bind/camera-bind.sh
+++ camera-bind/camera-bind.sh
@@ -19,10 +19,8 @@ case $OS in
 esac
 
 isCameraOnMac(){
-    cameraActivityLog=$(log show --predicate 'subsystem contains "com.apple.UVCExtension" and composedMessage contains "Post PowerLog"' --last 3m)
-    lastSixChars=${cameraActivityLog: -6}
-    cameraStatus=$(echo $lastSixChars | tr -cd '[:alnum:]._-')
-    [ "$cameraStatus" = "Off" ]; isCameraOn=$?
+    cameraStatus=$(log show --predicate 'subsystem contains "com.apple.UVCExtension" and composedMessage contains "Post PowerLog"' --last 3m | grep -E -o '"VDCAssistant_Power_State" = [a-zA-Z]+' | tail -1 | awk '{print $3}')
+    [ "$cameraStatus" = "On" ] && isCameraOn=1 || isCameraOn=0
     echo "$isCameraOn"
 }
 
