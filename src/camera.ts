2dd9b50d399b76ed18b7952613b405ac01d0c1a6
netoabel <abel.neto@gmail.com>
2024-01-28 00:41:41 -0300

    Adds linux support to the Node.js implementation

--- src/camera.ts
+++ src/camera.ts
@@ -1,24 +1,21 @@
 import { logger } from "./util/logger/logger";
 import { spawn, ChildProcessWithoutNullStreams } from "child_process";
+import os from "os";
 
 interface CameraLogCallbacks {
   onData?: (data: string) => void;
   onError?: (error: Error) => void;
 }
 
-function watchCameraLogs(callbacks: CameraLogCallbacks): void {
-  const logs: ChildProcessWithoutNullStreams = spawn("log", [
-    "stream",
-    "--predicate",
-    `subsystem contains "com.apple.UVCExtension" and composedMessage contains "Post PowerLog"`,
-  ]);
+function watchCameraLogsMac(callbacks: CameraLogCallbacks): void {
+  const logs = spawnCameraLogProcessMac();
 
   logs.stdout.setEncoding("utf8");
   logs.stdout.on("data", (data: string) => {
-    const regex: RegExp = /"VDCAssistant_Power_State"\s*=\s*([A-Za-z]+)/;
-    const match: RegExpExecArray | null = regex.exec(data);
+    const regex = /"VDCAssistant_Power_State"\s*=\s*([A-Za-z]+)/;
+    const match = regex.exec(data);
     if (match) {
-      const cameraState: string = match[1];
+      const cameraState = match[1];
       if (callbacks.onData) {
         callbacks.onData(cameraState);
       }
@@ -35,6 +32,57 @@ function watchCameraLogs(callbacks: CameraLogCallbacks): void {
   logs.on("exit", (code: any) => {
     logger.error("child process exited with code " + code.toString());
   });
+}
+
+function watchCameraLogsLinux(callbacks: CameraLogCallbacks): void {
+  const DEVICE_NAME = process.env.KEYCAM_DEVICE_NAME || "video0";
+  const logs = spawnCameraLogProcessLinux(DEVICE_NAME);
+
+  logs.stdout.setEncoding("utf8");
+  logs.stdout.on("data", (data: string) => {
+    const cameraState = data.indexOf(DEVICE_NAME) === -1 ? "Off" : "On";
+    if (callbacks.onData) {
+      callbacks.onData(cameraState);
+    }
+  });
+
+  logs.stderr.setEncoding("utf8");
+  logs.stderr.on("data", (error: Error) => {
+    if (callbacks.onError) {
+      callbacks.onError(error);
+    }
+  });
+
+  logs.on("exit", (code: any) => {
+    logger.error("child process exited with code " + code.toString());
+  });
+}
+
+function watchCameraLogs(callbacks: CameraLogCallbacks): void {
+  const hostOS = os.platform();
+
+  switch (hostOS) {
+    case "darwin":
+      watchCameraLogsMac(callbacks);
+      break;
+    case "linux":
+      watchCameraLogsLinux(callbacks);
+      break;
+  }
+}
+
+function spawnCameraLogProcessMac(): ChildProcessWithoutNullStreams {
+  return spawn("log", [
+    "stream",
+    "--predicate",
+    `subsystem contains "com.apple.UVCExtension" and composedMessage contains "Post PowerLog"`,
+  ]);
+}
+
+function spawnCameraLogProcessLinux(
+  deviceName: string
+): ChildProcessWithoutNullStreams {
+  return spawn("lsof", ["-r", "1", `/dev/${deviceName}`]);
 }
 
 export { watchCameraLogs };
