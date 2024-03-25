e2c7bc57df90edbd589e2c9b7a8963ee3df57349
netoabel <abel.neto@gmail.com>
2024-03-24 21:55:19 -0300

    Implements retry on request failure

--- src/keylight.ts
+++ src/keylight.ts
@@ -1,6 +1,21 @@
 import axios from "axios";
+import axiosRetry from "axios-retry";
 import * as http from "http";
 import { logger } from "./util/logger/logger";
+
+const RETRY_BASE_INTERVAL = 1500;
+const MAX_RETRIES = 5;
+
+axiosRetry(axios, {
+  retries: MAX_RETRIES,
+  retryDelay: (retryCount) => {
+    return RETRY_BASE_INTERVAL * retryCount;
+  },
+  onRetry: (retryCount, error) => {
+    logger.error(`Request to keylight failed. ${error.cause}`);
+    logger.info(`Retrying (${retryCount})...`);
+  },
+});
 
 const agentForHttp4: http.Agent = new http.Agent({ family: 4 });
 const keylightUrl: string =
