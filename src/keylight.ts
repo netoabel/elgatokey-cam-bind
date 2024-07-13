c2f71131c13cfd04976b7eeec9931f447cf8098f
netoabel <abel.neto@gmail.com>
2024-07-13 17:08:59 -0300

    Adds a timeout for http requests to the keylight

--- src/keylight.ts
+++ src/keylight.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 import * as http from "http";
 import { logger } from "./util/logger/logger";
 
+const REQUEST_TIMEOUT_MS = 3000;
+
 const agentForHttp4: http.Agent = new http.Agent({ family: 4 });
 const keylightUrl: string =
   "http://elgato-key-light-air-ec6e.local.:9123/elgato/lights";
@@ -10,7 +12,7 @@ async function setState(state: boolean): Promise<void> {
   await axios.put(
     keylightUrl,
     { lights: [{ on: state }] },
-    { httpAgent: agentForHttp4 },
+    { httpAgent: agentForHttp4, timeout: REQUEST_TIMEOUT_MS },
   );
 }
 
