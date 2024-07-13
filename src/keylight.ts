cecc6e7c5e6c55e114f014e5938a91d0ab221ceb
netoabel <abel.neto@gmail.com>
2024-07-13 17:10:24 -0300

    minor refactoring in keylight.js

--- src/keylight.ts
+++ src/keylight.ts
@@ -3,14 +3,14 @@ import * as http from "http";
 import { logger } from "./util/logger/logger";
 
 const REQUEST_TIMEOUT_MS = 3000;
+const KEYLIGHT_URL =
+  "http://elgato-key-light-air-ec6e.local.:9123/elgato/lights";
 
 const agentForHttp4: http.Agent = new http.Agent({ family: 4 });
-const keylightUrl: string =
-  "http://elgato-key-light-air-ec6e.local.:9123/elgato/lights";
 
 async function setState(state: boolean): Promise<void> {
   await axios.put(
-    keylightUrl,
+    KEYLIGHT_URL,
     { lights: [{ on: state }] },
     { httpAgent: agentForHttp4, timeout: REQUEST_TIMEOUT_MS },
   );
@@ -26,7 +26,7 @@ async function getCurrentState(): Promise<boolean | void> {
   let state: boolean | undefined;
 
   try {
-    const res = await axios.get(keylightUrl, { httpAgent: agentForHttp4 });
+    const res = await axios.get(KEYLIGHT_URL, { httpAgent: agentForHttp4 });
     state = !!+res?.data?.lights[0]?.on;
   } catch (err: any) {
     logger.error(err.message);
